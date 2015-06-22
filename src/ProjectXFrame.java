1bc9c4a54b83eaf0cad25cbdb8745cd847d395e6
Aubtin Samai <aubtins@gmail.com>
2015-06-21 23:13:53 -0700

    ProjectX

---
+++ src/ProjectXFrame.java
@@ -0,0 +1,19 @@
+//GUI
+
+import javax.swing.JFrame;
+
+
+public class ProjectXFrame extends JFrame
+{
+	public static void main(String[] args)
+	{
+			ProjectXFrame myX = new ProjectXFrame();
+			myX.setVisible(true);
+	}
+	
+	public ProjectXFrame()
+	{
+		setSize(400,500);
+		setTitle("Project X");
+	}
+}
