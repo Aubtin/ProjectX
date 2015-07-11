9a6b9dd0931b7391c09c24eada83cef5fabcb060
Aubtin Samai <aubtins@gmail.com>
2015-07-11 05:22:32 -0700

    SQL
    Added mysql.

--- ProjectXWeb/src/com/projectx/server/ProjectXServlet.java
+++ ProjectXWeb/src/com/projectx/server/ProjectXServlet.java
@@ -1,14 +1,6 @@
 package com.projectx.server;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
 import java.util.Scanner;
 
 import javax.servlet.ServletException;
@@ -17,40 +9,112 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.json.JSONObject;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 
 /**
  * Servlet implementation class ProjectXServlet
+ * MySQL on AWS Oregon	
+ * DB Instance Identifier: InternProject
+ * DB Name: BlogDB
+ * Master Username: dbaccess
+ * Master Password: InternProject123
  */
 @WebServlet("/ProjectXServlet")
-public class ProjectXServlet extends HttpServlet {
-	
-	private String requestBody;
-	private String directory = "C:/Users/Aubtin/Documents/workspaceFahrzin/ProjectX/";
+public class ProjectXServlet extends HttpServlet 
+{
+
+	// JDBC driver name and database URL
+    	static final String JDBC_DRIVER="com.mysql.jdbc.Driver"; 
+    	static final String DB_URL="jdbc:mysql://internproject.cotpco4fkejc.us-west-2.rds.amazonaws.com:3306/BlogDB";
+
+    	//  Database credentials
+    	static final String USER = "dbaccess";
+    	static final String PASS = "InternProject123";
 	
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ProjectXServlet() 
-    {
-        super();
-        
-    }
+
+   	/**
+   	* @see HttpServlet#HttpServlet()
+   	*/
+   	public ProjectXServlet() 
+   	{
+        	super();
+    	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	* @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	*/
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{
-		//throw new ServletException("422 Unprocessable Entity");
-		
-		requestBody = readFile();
-		System.out.println("GET: " + requestBody);
-        request.setAttribute("message", requestBody); // This will be available as ${message}
-        request.getRequestDispatcher("/index.jsp").forward(request, response);
+		Connection conn = null;  
+		Statement stmt = null;
+		ResultSet rs = null;
+
+		try
+    		{
+			Class.forName(JDBC_DRIVER); 
+
+			// Open a connection
+	    		conn = DriverManager.getConnection(DB_URL,USER,PASS);
+
+	    		// Execute SQL query
+    			stmt = conn.createStatement();
+
+    			String sql = "SELECT id, note FROM posts WHERE id=1";
+	    		rs = stmt.executeQuery(sql);
+
+	    		// Extract data from result set
+    			while(rs.next())
+    			{
+               			//Retrieve by column name
+                		int id  = rs.getInt("id");
+             			String note = rs.getString("note");
+
+                		//Format for JSP
+                		request.setAttribute("message", note); // This will be available as ${message}
+                		request.getRequestDispatcher("/index.jsp").forward(request, response);
+    			}
+             		// Clean-up environment
+             		rs.close();
+             		stmt.close();
+             		conn.close();
+    		}
+    		catch(SQLException se)
+    		{
+    			//Handle errors for JDBC
+    			se.printStackTrace();
+    		}
+		catch(Exception e)
+    		{
+    			//Handle errors for Class.forName
+    			e.printStackTrace();
+    		}
+    		finally
+    		{
+             		//finally block used to close resources
+             		try
+             		{
+                		if(stmt!=null)
+                  			stmt.close();
+             		}
+             		catch(SQLException se2)
+             		{
+             		}
+             		try
+             		{
+            	 		if(conn!=null)
+            		 		conn.close();
+             		}
+             		catch(SQLException se)
+            		{
+            	 		se.printStackTrace();
+            		}
+    		}
 	}
 
 	/**
@@ -58,89 +122,82 @@ public class ProjectXServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{
-		PrintWriter out = response.getWriter();
+		Connection conn = null;  
+		Statement stmt = null;
 
+		String requestBody;
 
-		requestBody = extractPostRequestBody(request).substring(7);
-		
-//		 try{
+		try
+    		{
+			Class.forName(JDBC_DRIVER);
 
-//		        File file = new File("c:/example/filedata.txt");
-//		        FileReader fileReader = new FileReader(file);
-//		        BufferedReader bufferedReader = new BufferedReader(fileReader);
-//		        String temp = bufferedReader.readLine();
-   
-		
-			 	System.out.println(requestBody);
-			 	writeFile(requestBody);
-			 	
-				doGet(request, response);
+    			// Open a connection
+    			conn = DriverManager.getConnection(DB_URL,USER,PASS);
 
-//			 	System.out.println("File Read As: " + readFile());
-		        //Clear first.
-//		        fileWriter.println("");
-//		        fileWriter.println( requestBody );
-//		        System.out.println("file saved");
-//		        fileWriter.close(); 
-//		        fileReader.close();
-//			}
-//		    catch(FileNotFoundException fnfe) {    
-//
-//		  }  
-		/**
-		//Create JSON object and add info.
-		JSONObject userText = new JSONObject();
-		userText.put("myText", requestBody);
-		
-	
-		//Print JSON
-		out.println(userText.toString());
-		**/
+	    		// Execute SQL query
+    			stmt = conn.createStatement();
+    		
+    			//Get text from textarea.
+			requestBody = extractPostRequestBody(request).substring(7);
+			
+			try 
+			{
+				stmt.executeUpdate("UPDATE posts SET note='" + requestBody + "' WHERE id=1");
+			}
+			catch (SQLException e) 
+			{
+				e.printStackTrace();
+			}
+
+			// Clean-up environment
+             		stmt.close();
+             		conn.close();
+    		}
+    		catch(SQLException se)
+    		{
+    			//Handle errors for JDBC
+    			se.printStackTrace();
+    		}
+		catch(Exception e)
+    		{
+    			//Handle errors for Class.forName
+    			e.printStackTrace();
+    		}
+    		finally
+    		{	
+             		//finally block used to close resources
+             		try
+             		{
+               			if(stmt!=null)
+                   			stmt.close();
+             		}
+             		catch(SQLException se2)
+             		{
+             		}
+
+             		try
+            		{
+            	 		if(conn!=null)
+            		 		conn.close();
+             		}
+             		catch(SQLException se)
+             		{
+            	 		se.printStackTrace();
+             		}
+    		}
+
+		//Retrieve info again.
+		doGet(request, response);
 	}
 
 	//Capture the body from the POST, and return as a string.
 	static String extractPostRequestBody(HttpServletRequest request) throws IOException 
 	{
-	    if ("POST".equalsIgnoreCase(request.getMethod())) {
-	        Scanner s = new Scanner(request.getInputStream(), "UTF-8").useDelimiter("\\A");
-	        return s.hasNext() ? s.next() : "";
-	    }
-	    return "";
-	}
-
-	//Read the file
-	public String readFile() throws IOException 
-	{
-		File writerFile = new File(directory + "textdata.txt");
-		if(!writerFile.exists())
-			  writerFile.createNewFile();
-		
-	    BufferedReader br = new BufferedReader(new FileReader(writerFile));
-	    try {
-	        StringBuilder sb = new StringBuilder();
-	        String line = br.readLine();
-
-	        while (line != null) {
-	            sb.append(line);
-	            sb.append("\n");
-	            line = br.readLine();
-	        }
-	        return sb.toString();
-	    } finally {
-	        br.close();
-	    }
-	}
-	
-	//Write to file
-	public void writeFile(String text) throws IOException
-	{
-		File writerFile = new File(directory + "textdata.txt");
-		if(!writerFile.exists())
-			  writerFile.createNewFile();
-		
-	 	PrintWriter writer = new PrintWriter(writerFile, "UTF-8");
-	 	writer.println(text);
-	 	writer.close();
-	 	System.out.println("File Saved.");
+		if ("POST".equalsIgnoreCase(request.getMethod())) 
+		{
+	        	Scanner s = new Scanner(request.getInputStream(), "UTF-8").useDelimiter("\\A");
+	        	return s.hasNext() ? s.next() : "";
+	    	}
+	    	return ""; 
 	}
 }
---
+++ SQL/poststTable.sql
@@ -0,0 +1,5 @@
+CREATE TABLE posts(
+id int NOT NULL AUTO_INCREMENT,
+note varchar(500),
+PRIMARY KEY(id)
+)
